64323b59c5aad45b02c3570b751fe925c2196e9c
gun587330 <gun5873@naver.com>
2025-01-07 17:43:53 +0900

    section02 reference를 참고해서 수정

--- pages/portfolio/section01/index.js
+++ pages/portfolio/section01/index.js
@@ -1,29 +1,27 @@
 import {
+    WriterWrapper, 
     Wrapper, 
-    Wrapper__header, 
-    Wrapper__title, 
-    Author,
+    Title,
+    InputWrapper,
     Writer,
     Password,
     Word, 
-    Title, 
     Subject,
-    Content, 
-    Contents, 
+    Contents,  
+    Zipcode, 
+    AddressButton, 
     Address, 
-    AddressMail__textBox, 
-    Address__Button, 
-    Address__textBox, 
-    Link,
-    Link__textBox, 
-    Update, 
+    Youtube, 
+    UploadWrapper, 
     UpdatePicture, 
     Picture, 
-    Setting, 
-    Setting__radio, 
-    Wrapper__footer, 
-    Enroll__button, 
-    Wrapper__body,
+    SettingWrapper,
+    RadioButton,
+    RadioWord, 
+    ButtonWrapper, 
+    EnrollButton, 
+    ZipcodeWrapper,
+    ErrorMessage,
 } from '../../../styles/section05'
 import { useState } from "react"
 
@@ -31,94 +29,133 @@ export default function PortfolioPage(){
     //Javascript Zone
 
     //State 선언
-    const [writer, setWriter] = useState("")
-    const [password, setPassword] = useState("")
-    const [subject, setSubject] = useState("")
-    const [contents, setContents] = useState("")
+    const [writer, setWriter] = useState("");
+    const [password, setPassword] = useState("");
+    const [subject, setSubject] = useState("");
+    const [contents, setContents] = useState("");
+
+    //Error에 관한 State 선언
+    const [writerError, setWriterError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [subjectError, setSubjectError] = useState("");
+    const [contentsError, setContentsError] = useState("");
 
     function onChangeWriter(event){
         // 스테이트에 데이턱가 잘 저장되었는지 체크용
         // console.log(event.target.value)
-        setWriter(event.target.value)
-    }
+        setWriter(event.target.value);
+        if(event.target.value !== ""){
+            setWriterError("");
+        }
+    };
 
     function onChangePassword(event){
-        setPassword(event.target.value)
-    }
+        setPassword(event.target.value);
+        if (event.target.value){
+            setPasswordError("");
+        }
+    };
 
     function onChangeSubject(event){
-        setSubject(event.target.value)
-    }
+        setSubject(event.target.value);
+        if (event.target.value){
+            setSubjectError("");
+        }
+    };
 
     function onChangeContents(event){
-        setContents(event.target.value)
-    }
+        setContents(event.target.value);
+        if (event.target.value){
+            setContentsError("");
+        }
+    };
+
+    const onClickEnroll = () => {
+        if(!writer){
+            setWriterError("작성자를 입력해주세요.");
+        }
+        if(!(password)){
+            setPasswordError("비밀번호를 입력해주세요.");
+        }
+        if(!(subject)){
+            setSubjectError("제목을 입력해주세요.");
+        }
+        if(!(contents)){
+            setContentsError("내용을 입력해주세요.");
+        }
+        if(writer && password && subject && contents){
+            alert("가입을 환영합니다!")
+        }
+    };
 
     return(
         // HTML Zone
         <Wrapper>
-            <Wrapper__title>게시물 등록</Wrapper__title>
-            <Wrapper__header>
+            <Title>게시물 등록</Title>
 
-                <Author>
+            <WriterWrapper>
+                <InputWrapper>
                     <Word>작성자</Word>
                     <Writer type="text" placeholder="이름을 적어주세요." onChange={onChangeWriter}/>
-                </Author>
+                    <ErrorMessage>{writerError}</ErrorMessage>
+                </InputWrapper>
 
-                <Author>
+                <InputWrapper>
                     <Word>비밀번호</Word>
                     <Password type="password" placeholder="바말번호를 입력해주세요." onChange={onChangePassword} />
-                </Author>
+                    <ErrorMessage>{passwordError}</ErrorMessage>
+                </InputWrapper>
+            </WriterWrapper>
 
-            </Wrapper__header>
+            <InputWrapper>
+                <Word>제목</Word>
+                <Subject placeholder="제목을 작성해주세요." onChange={onChangeSubject}/>
+                <ErrorMessage>{subjectError}</ErrorMessage>
+            </InputWrapper>
 
-            <Wrapper__body>
-                <Title>
-                    <Word>제목</Word>
-                    <Subject placeholder="제목을 작성해주세요." onChange={onChangeSubject}/>
-                </Title>
+            <InputWrapper>
+                <Word>내용</Word>
+                <Contents placeholder="내용을 작성해주세요." onChange={onChangeContents}/>
+                <ErrorMessage>{contentsError}</ErrorMessage>
+            </InputWrapper>
 
-                <Content>
-                    <Word>내용</Word>
-                    <Contents placeholder="내용을 작성해주세요." onChange={onChangeContents}/>
-                </Content>
+            <InputWrapper>
+                <Word>주소</Word>
+                <ZipcodeWrapper>
+                    <Zipcode placeholder='07250'/>
+                    <AddressButton>우편번호 검색</AddressButton>
+                </ZipcodeWrapper>
+                <Address></Address>
+                <Address></Address>
+            </InputWrapper>
 
-                <Address>
-                    <Word>주소</Word>
-                    <div className="mailName">
-                        <AddressMail__textBox placeholder='07250'></AddressMail__textBox>
-                        <Address__Button>우편번호 검색</Address__Button>
-                    </div>
-                    <Address__textBox></Address__textBox>
-                    <Address__textBox></Address__textBox>
-                </Address>
+            <InputWrapper>
+                <Word>유튜브</Word>
+                <Youtube placeholder="링크를 복사해주세요."/>
+            </InputWrapper>
 
-                <Link>
-                    <Word>유튜브</Word>
-                    <Link__textBox placeholder="링크를 복사해주세요." />
-                </Link>
+            <UploadWrapper>
+                <Word>사진 첨부</Word>
+                <UpdatePicture>
+                    <Picture>+</Picture>
+                    <Picture>+</Picture>
+                    <Picture>+</Picture>
+                </UpdatePicture>
+            </UploadWrapper>
 
-                <Update>
-                    <Word>사진 첨부</Word>
-                    <UpdatePicture>
-                        <Picture></Picture>
-                        <Picture></Picture>
-                        <Picture></Picture>
-                    </UpdatePicture>
-                </Update>
+            <SettingWrapper>
+                <Word>메인 설정</Word>
+                {/* <input type="radio" className="mainSetting" name="mainSetting"/> 유튜브
+                <input type="radio" className="mainSetting" name="mainSetting"/> 사진 */}
+                <RadioButton type="radio" id="youtube" name="radio-button" />
+                <RadioWord htmlFor="youtube">유튜브</RadioWord>
+                <RadioButton type="radio" id="image" name="radio-button" />
+                <RadioWord htmlFor="image">사진</RadioWord>
+            </SettingWrapper>
 
-                <Setting>
-                    <Word>메인 설정</Word>
-                    <Setting__radio>
-                        <input type="radio" className="mainSetting" name="mainSetting"/> 유튜브
-                        <input type="radio" className="mainSetting" name="mainSetting"/> 사진
-                    </Setting__radio>
-                </Setting>
-            </Wrapper__body>
-
-            <Wrapper__footer>
-                <Enroll__button>등록하기</Enroll__button>
-            </Wrapper__footer>
+            <ButtonWrapper>
+                <EnrollButton onClick={onClickEnroll}>등록하기</EnrollButton>
+            </ButtonWrapper>
         </Wrapper>
     )
 }
--- styles/section05.js
+++ styles/section05.js
@@ -5,121 +5,126 @@ import styled from '@emotion/styled'
 export const Wrapper = styled.div`
     width: 1200px;
     // height: 1847px;          // 높이를 고정하면, 나중에 글을 쓸 때 모양이 오바될 수 있음
-    border: 1px solid #BDBDBD;
-    display: flex;
+    border: 1px solid black;
     margin: 100px;
-    padding: 80px;
+    // padding: 80px;
+    padding-top: 80px;
+    padding-bottom: 100px;
+    padding-left: 102px;
+    padding-right: 102px;
+    display: flex;
     flex-direction: column;
     align-items: center;
     box-shadow: 0px 0px 10px gray;
     border-radius: 15px;
-`
-// reference: Title
-export const Wrapper__title = styled.div`
+`;
+
+export const Title = styled.div`
     font-family: Arial, Helvetica, sans-serif;
     font-size : 34px;
     font-weight : bold;
-    margin-bottom: 80px;
-`
+    // margin-bottom: 80px;
+`;
 
-export const Writer = styled.input`
-    height: 52px;
-    padding-left: 10px;
-    border: 1px solid #bdbdbd;
-`
-
-export const Password = styled.input`
-    height: 52px;
-    padding-left: 10px;
-    border: 1px solid #bdbdbd;
-`
-
-// reference: WriterWrapper
-export const Wrapper__header = styled.div`
+export const WriterWrapper = styled.div`
+    // width: 100%;
     width: 996px;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-`
+    padding-top: 40px;
+`;
 
-// reference: 
-export const Author = styled.div`
+export const Writer = styled.input`
     width: 486px;
-    height: 92px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-bottom: 40px;
-`
+    height: 52px;
+    padding-left: 16px;
+    border: 1px solid #bdbdbd;
+`;
 
-export const Word = styled.span`
+export const Password = styled.input`
+    width: 486px;
+    height: 52px;
+    padding-left: 16px;
+    border: 1px solid #bdbdbd;
+`;
+
+export const Word = styled.div`
     font-size: 16px;
     font-weight: 400;
-`
+    padding-bottom: 16px;
+`;
 
-export const Title = styled.div`
-    width: 996px;
-    height: 92px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-bottom: 50px;
-`
+export const InputWrapper = styled.div`
+    padding-top: 40px;
+`;
 
 export const Subject = styled.input`
+    // width: 100%;
+    width: 996px;
     height: 52px;
     padding-left: 10px;
     border: 1px solid #bdbdbd;
-`
+`;
 
-export const Wrapper__body = styled.div`
-    margin-bottom: 50px;
-`
+// export const Wrapper__body = styled.div`
+//     margin-bottom: 50px;
+// `
 
-export const Content = styled.div`
-    width: 996px;
-    height: 520px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-bottom: 30px;
-`
+// export const Content = styled.div`
+//     width: 996px;
+//     height: 520px;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     margin-bottom: 30px;
+// `
 
 export const Contents = styled.textarea`
+    width: 996px;
     height: 480px;
     border: 1px solid #BDBDBD;
     padding-top: 10px;
-    padding-left: 10px;
-`
+    padding-left: 16px;
+`;
 
-export const Address = styled.div`
-    width: 996px;
-    height: 228px;
+// export const Address = styled.div`
+//     width: 996px;
+//     height: 228px;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-between;
+//     margin-bottom: 30px;
+// 
+
+export const ZipcodeWrapper = styled.div`
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-bottom: 30px;
-`
-export const AddressMail__textBox = styled.input`
+    flex-direction: row;
+`;
+
+export const Zipcode = styled.input`
     width: 77px;
     height: 52px;
     margin-right: 20px;
     padding-left: 20px;
     border: 1px solid #BDBDBD;
-`
+`;
 
-export const Address__textBox = styled.input`
+export const Address = styled.input`
+    width: 996px;
     height: 52px;
     border: 1px solid #BDBDBD;
-`
+    margin-top: 16px;
+    padding-left: 16px;
+`;
 
-export const Address__Button = styled.button`
+export const AddressButton = styled.button`
     width: 124px;
     height: 52px;
     background-color: black;
     color: white;
     border: 1px solid black;
-`
+`;
 
 export const Link = styled.div`
     width: 996px;
@@ -128,60 +133,79 @@ export const Link = styled.div`
     flex-direction: column;
     justify-content: space-between;
     margin-bottom: 30px;
-`
+`;
 
-export const Link__textBox = styled.input`
+export const Youtube = styled.input`
+    width: 996px;
     height: 52px;
     padding-left: 20px;
     border: 1px solid #BDBDBD;
-`
+`;
 
-export const Update = styled.div`
-    width: 282px;
-    height: 118px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+export const UploadWrapper = styled.div`
+    width: 100%;
+    padding-top: 40px;
     margin-bottom: 30px;
-`
+`;
 
 export const UpdatePicture = styled.div`
     height: 78px;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-`
+    justify-content: flex-start;
+`;
 
 export const Picture = styled.div`
     width: 78px;
     height: 78px;
-    background-color: gray; 
-`
-
-export const Setting = styled.div`
-    width: 159px;
-    height: 64px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-`
-
-export const Setting__radio = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-`
-
-export const Wrapper__footer = styled.div`
-    width: 996px;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
-`
+    background-color: gray;
+    margin-right: 10px;
+`;
 
-export const Enroll__button = styled.button`
+export const SettingWrapper = styled.div`
+    width: 996px;
+    padding-top: 40px;
+`;
+
+export const RadioButton = styled.input`
+  cursor: pointer;
+`;
+
+export const RadioWord = styled.label`
+  margin-left: 8px;
+  margin-right: 20px;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+export const ButtonWrapper = styled.div`
+    width: 996px;
+    padding-top: 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const EnrollButton = styled.button`
     width: 179px;
     height: 52px;
-    background-color: #FFD600;
+    border: none;
+    font-size: 16px;
+    font-weight: 500;
+    margin-left: 12px;
+    margin-right: 12px;
+    cursor: pointer;
+    background: yellow;
+`;
+
+export const ErrorMessage = styled.div`
+    color: red;
+    padding-top: 10px;
+    font-size: 14px;
+
 `
